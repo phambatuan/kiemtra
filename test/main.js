84e8b2c09f3072ee2d15687e76c5d8b719d12c5d
phambatuan <49505516+phambatuan@users.noreply.github.com>
2022-05-31 22:42:23 +0700

    nop bai test

---
+++ test/main.js
@@ -0,0 +1,39 @@
+let a  = parseInt(prompt("mời bạn nhập số a"));
+let b = parseInt(prompt("mời bạn nhập số b"));
+
+let n  = parseInt(prompt("mời bạn nhập số dongf n"));
+for(i = 1;i<=n;++i)
+{
+  for(j=1;j<=i;j++){
+    document.write("*");
+  }
+  document.write("<br/>");
+  }
+
+
+
+function sNT(num1, num2) {
+    let numstr = "";
+    for (let i = num1; i <= num2; i++) {
+        if (i == 2 || i == 3) {
+            numstr += i + ", ";
+        }
+        if (i > 3) {
+            for (j = 2; j < i; a++) {
+                if (i % j != 0) {
+                    numstr += i + ", ";
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    alert("Số nguyên tố trong khoảng hai số là: " + numstr);
+}
+
+if (a < b) {
+    sNT(a, b);
+} else {
+    sNT(b, a);
+}
